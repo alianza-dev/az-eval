4333fe1b236d7443b40c01f9982649d8fcaf012b
Justin McMurdie <mcmurdiej@gmail.com>
2015-09-01 16:32:56 -0600

    adding index.js to the dist folder for debugging

---
+++ dist/index.js
@@ -0,0 +1,105 @@
+//! az-eval version 1.0.0 built with ♥ by Justin McMurdie <mcmurdiej@gmail.com> (http://mcmurdie.net/) && Kent C. Dodds <kent@doddsfamily.us> (http://kentcdodds.com/) (ó ì_í)=óò=(ì_í ò)
+
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("angular"));
+	else if(typeof define === 'function' && define.amd)
+		define(["angular"], factory);
+	else if(typeof exports === 'object')
+		exports["azEval"] = factory(require("angular"));
+	else
+		root["azEval"] = factory(root["angular"]);
+})(this, function(__WEBPACK_EXTERNAL_MODULE_1__) {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+	var _angular = __webpack_require__(1);
+
+	var _angular2 = _interopRequireDefault(_angular);
+
+	var ngModuleName = 'azEval';
+	var ngModule = _angular2['default'].module(ngModuleName, []);
+
+	exports['default'] = ngModule.factory('azEval', azEvalFactory).name;
+	// <-- exporting the module name
+
+	// @ngInject
+	function azEvalFactory($parse) {
+
+	  return function azEval(expression, item) {
+	    var value = undefined;
+	    if (typeof expression === 'function') {
+	      value = expression(item);
+	    } else if (typeof expression === 'string') {
+	      value = $parse(expression)(item);
+	    } else {
+	      console.log('123');
+	      throw new Error('azEval only accepts functions and expression strings');
+	    }
+	    return value;
+	  };
+	}
+	azEvalFactory.$inject = ["$parse"];
+	module.exports = exports['default'];
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	module.exports = __WEBPACK_EXTERNAL_MODULE_1__;
+
+/***/ }
+/******/ ])
+});
+;
